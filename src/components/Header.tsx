2e716cd776bff93d049178a29c319e2fdec9d49a
Justin Sillou <justinsillou@gmail.com>
2025-10-29 21:39:23 +0100

    update follow v2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <header className="border-b border-gray-100">
+    <header className="border-b border-gray-100 z-1">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
