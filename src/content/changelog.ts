fbeb15c954b1a06332fdbaeefe64404fb9cd9c72
Justin <justinsillou@gmail.com>
2026-08-19 15:58:45 +0200

    feat: update dependencies and implement Tailwind CSS
    - Updated Astro to version 7.2.3 and added Tailwind CSS with Vite support.
    - Removed unused favicon.svg and updated favicon.ico.
    - Refactored index.astro to use BaseLayout and Tailwind CSS for styling.
    - Simplified global styles by integrating Tailwind CSS.
    - Added BarryAllen component for Konami code Easter egg functionality.
    - Created BaseHeader and BaseFooter components for consistent layout.
    - Implemented changelog feature with ChangelogTimeline component.
    - Added changelog content for version history.
    - Created 404 page with interactive buttons for user navigation.
    - Configured Tailwind CSS for dark mode support.

---
+++ src/content/changelog.ts
@@ -0,0 +1,40 @@
+export const changelog = [
+    {
+        version: "0.9.3",
+        date: "18/08/2026",
+        title: "Ajout d'un menu burger [WIP]",
+        changes: [
+            "Ajout d'un menu burger",
+            "Création des idées de page CV, Blog, ...",
+            "↑ ↑ ↓ ↓ ← → ← → B A"
+        ],
+    },
+    {
+        version: "0.9.2",
+        date: "14/07/2026",
+        title: "Ça se précise !",
+        changes: [
+            "Nouveau design sobre avec mode clair/sombre",
+            "Ajout d'une page de changelog",
+            "Passage à Tailwind",
+            "Page 404",
+            "Pas encore de contenu, mais ça va venir !",
+        ],
+    },
+    {
+        version: "0.9.1",
+        date: "08/06/2026",
+        title: "En cours de développement",
+        changes: [
+            "Les idées cogitent, mais le site est encore en construction. Il n'y a pas grand chose à voir pour le moment.",
+        ],
+    },
+    {
+        version: "0.9.0",
+        date: "01/04/2026",
+        title: "Il est temps de se refaire une beauté",
+        changes: [
+            "Non ce n'était pas une blague du 1er Avril, il a été convenu de repartir de 0. Dans le but de créer un site plus simple et plus rapide",
+        ]
+    },
+];
